7e5f159237b64aeab8109c6999142b5f7e0f7d32
lufangzhou <735417031@qq.com>
2017-12-08 17:46:09 +0800

    *ngFor = "let hero of heroes"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  template: '<h1>{{title}}</h1>' +
-            '<h2>{{hero}} details!</h2>',
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>{{title}}</h1>
+    <div>
+      <label>nowName:</label>
+      <span>{{hero.name}}</span>
+    </div>
+    <div>
+      <span>pse write name↓</span>
+      <label for = "name">name:</label>
+      <input id = "name" type="text" placeholder="name" [(ngModel)]="hero.name">
+    </div>
+    <hr>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span id = "{{hero.id}}">{{hero.name}}</span>
+      </li>
+    </ul>
+  `
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  title = 'Tour of Heroes by Tog';
+  hero: Hero = {
+    id: 1,
+    name: 'lfz'
+  };
+  heroes = HEROES;
 }
+const HEROES:  Hero[] = [
+  {id: 1, name: '11'},
+  {id: 2, name: '22'},
+  {id: 3, name: '33'},
+  {id: 4, name: '44'},
+  {id: 5, name: '55'},
+  {id: 6, name: '66'},
+  {id: 7, name: '77'},
+  {id: 8, name: '88'},
+  {id: 9, name: '99'},
+  {id: 10, name: '1010'}
+];
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'; // <--导入表单参数
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
